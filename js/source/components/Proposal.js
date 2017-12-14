0f880c944204623746889d0b899ca9f2f37bb705
[dpetrini] <[d.pensator@gmail.com]>
2017-12-14 15:41:44 -0200

    proposal html sending OK

--- js/source/components/Proposal.js
+++ js/source/components/Proposal.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom'
+
+import { Email, Item, Span, A, renderEmail, Image } from 'react-html-email'
+
+
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { Button, Modal } from 'react-bootstrap';
 import html2canvas from '../extlib/html2canvas.min';
 import jsPDF from '../extlib/jspdf.min';
+
+// import '../extlib/cycle.js';
 
 import SolarBarChart from './Barchart'
 
@@ -23,13 +30,6 @@ class Proposal extends Component {
   // Fire redux action
   proposalButton() {
     var timeNow = new Date();
- 
-    //let chartSVG = ReactDOM.findDOMNode(this.currentChart.currentChart).children[0];
-    //console.log(chartSVG)
-    //let svgURL = new XMLSerializer().serializeToString(chartSVG);
-    //let svgBlob = new Blob([svgURL], {type: 'image/svg+xml;charset=utf-8'});
-    ////  let svgBlob = new Blob([chartSVG.outerHTML], {type: "text/html;charset=utf-8"});
-    //console.log(svgURL)
 
     // From here on will submit
     const proposalData	= {
@@ -57,6 +57,73 @@ class Proposal extends Component {
 
     }
   }
+
+
+  // Send email through Express server
+  sendEmail() {
+    var timeNow = new Date();
+
+    //console.log(this.myDiv)
+
+    const input = document.getElementById('divToPrint');
+    html2canvas(input)
+      .then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+
+        const contentHTML = renderEmail(
+          <Email title="Hello World!">
+            <Item align="center">
+              {this.costumerTextFirstPart()}
+            </Item>
+            <Item align="center">
+              <Image src={imgData} alt="grafico de consumo" width={600} height={280} />
+            </Item>
+            <Item align="center">
+              {this.costumerTextSecondPart()}
+            </Item>
+            <Item align="center">
+              <Span fontSize={20}>               
+                This is an example email made with:
+                <A href="https://github.com/chromakode/react-html-email">react-html-email</A>.
+              </Span>
+            </Item>
+          </Email>
+        )
+
+        // From here on will submit
+        const proposalData	= {
+          costumer_id: this.props.selectedCostumerId, // Send ID to find later
+          firstName: this.costumerName,
+          lastName:  this.costumerLastName,
+          email: this.costumerEmail,
+          avgConsumption: this.props.systemData.avgConsumption,
+          sysProposal: this.props.resultData[0].sysProposal,
+          panels: this.props.resultData[0].panels,
+          totalCost: this.props.resultData[0].totalCost,
+          payback: this.props.resultData[0].payback,
+          gerCost: this.props.systemData.gerCost,
+          avgCost: this.props.systemData.avgCost,
+          //contentHTML: JSON.stringify(JSON.decycle(this.myDiv)),
+          contentHTML: contentHTML,
+          status: 'sent',
+          date: timeNow.toISOString(),
+        };
+  
+        if (this.costumerName === '') {
+          console.log('Empty customer name for proposal. Not done.')
+          return;
+        } else {
+  
+          console.log(proposalData)
+  
+          this.props.onSendProposal(config.PROPOSAL_EMAIL_URL, proposalData);
+  
+        }
+
+  
+      });
+  }
+
 
   componentWillReceiveProps(newProps) {
  
@@ -167,7 +234,72 @@ class Proposal extends Component {
       });
   }
 
+  // First part of costumer text (before graph image)
+  costumerTextFirstPart() {
+    return (
+      <div id="firstpart" ref = {c => this.myDiv = c}>
+      
+      Oi {this.costumerName}, <br />
+      
+      Obrigado pelas informações <br />
+      
+      Pela conta podemos ver o perfil e inclusive que o maior consumo é nos meses de inverso. Provavelmente 
+       devido a aquecimento de agua  ou chuveiro elétrico. <br />
+        <br />
+      Considerando seus dados, para um consumo médio de
+       aproximadamente <strong> {this.props.systemData.avgConsumption} kWh/mês</strong>, estimamos um sistema
+       fotovoltaico de <strong> {this.props.resultData[0].sysProposal} kWp</strong>.<br />
+        <br />
+      Para construir esse sistema precisamos em média de <strong> {this.props.resultData[0].panels} placas 
+        solares</strong> de 320W (cada uma tem dimensão 1m x 2m).<br />
+        <br />
+      Para o projeto de seu sistema consideramos o uso de Microinversores, que são os componentes 
+       que transformam a energia solar em energia da rede. Os microinversores são mais seguros para casas 
+       e não requerem que se instale nenhum equipamento de alta tensão dentro da casa. Veja no anexo mais 
+       informações.<br />
+        <br />
+      O custo aproximado desse sistema 
+       é <strong>R$ {this.props.resultData[0].totalCost.toFixed(2).replace('.', ',')}</strong>, (depende 
+       de fatores de construção e orientação do telhado). Dependendo da ocasião podemos ter preços 
+       promocionais também.<br />
+        <br />
+      O tempo de retorno desse investimento é
+       aproximadamente <strong>{this.props.resultData[0].payback.toFixed(2).replace('.', ',')} anos</strong>, 
+       considerando a inflação e o desconto que terá da conta de energia, que passará a ser algo em 
+       torno de <strong>R$ {this.props.systemData.gerCost.toFixed(2).replace('.', ',')} </strong>, ao invés 
+       dos aproximadamente <strong>R$ {this.props.systemData.avgCost} atuais (média)</strong>.<br />
+      
+      Nesse preço ainda podemos ter promoções, depende um pouco da ocasião.<br />
+        <br />
+      E terá uma curva de produção como abaixo:<br />
+      </div>
+    );
+  }
   
+  // Content part after graph image (botton)
+  costumerTextSecondPart() {
+    return (
+      <div>
+              
+      Por favor visite nosso website para ter mais informações sobre energia solar:<br />
+
+        <a href="http://integrahausenergiasolar.com.br/energia-solar-para-residencias/">Energia Solar Para Residências</a>.
+        <br />
+        <a href="http://integrahausenergiasolar.com.br/vantagens-e-desvantagens-da-energia-solar/">Vantagens e Desvantagens da Energia Solar</a>.
+        <br />
+        <br />
+
+      Obrigado pelo contato.
+      Estamos a disposição para fazer uma visita e lhe explicar o funcionamento da energia solar pessoalmente.<br />
+        <br />
+        <br />
+      Atenciosamente,
+        <br />
+      </div>
+    );
+  }
+
+
   render () {
 
     // default empty names
@@ -179,72 +311,30 @@ class Proposal extends Component {
       if (element._id === this.props.selectedCostumerId) {
         this.costumerName = element.firstName;
         this.costumerLastName = element.lastName;
+        this.costumerEmail = element.email;
       }
     })
 
     return (
       <div className={'main-screen-body'} >
-        <div id="fulldoc">
+   
+        {this.costumerTextFirstPart()}
 
-Oi {this.costumerName}, <br />
+        <br />
 
-Obrigado pelas informações <br />
-
-Pela conta podemos ver o perfil e inclusive que o maior consumo é nos meses de inverso. Provavelmente 
- devido a aquecimento de agua  ou chuveiro elétrico. <br />
-          <br />
-Considerando seus dados, para um consumo médio de
- aproximadamente <strong> {this.props.systemData.avgConsumption} kWh/mês</strong>, estimamos um sistema
- fotovoltaico de <strong> {this.props.resultData[0].sysProposal} kWp</strong>.<br />
-          <br />
-Para construir esse sistema precisamos em média de <strong> {this.props.resultData[0].panels} placas 
-  solares</strong> de 320W (cada uma tem dimensão 1m x 2m).<br />
-          <br />
-Para o projeto de seu sistema consideramos o uso de Microinversores, que são os componentes 
- que transformam a energia solar em energia da rede. Os microinversores são mais seguros para casas 
- e não requerem que se instale nenhum equipamento de alta tensão dentro da casa. Veja no anexo mais 
- informações.<br />
-          <br />
-O custo aproximado desse sistema 
- é <strong>R$ {this.props.resultData[0].totalCost.toFixed(2).replace('.', ',')}</strong>, (depende 
- de fatores de construção e orientação do telhado). Dependendo da ocasião podemos ter preços 
- promocionais também.<br />
-          <br />
-O tempo de retorno desse investimento é
- aproximadamente <strong>{this.props.resultData[0].payback.toFixed(2).replace('.', ',')} anos</strong>, 
- considerando a inflação e o desconto que terá da conta de energia, que passará a ser algo em 
- torno de <strong>R$ {this.props.systemData.gerCost.toFixed(2).replace('.', ',')} </strong>, ao invés 
- dos aproximadamente <strong>R$ {this.props.systemData.avgCost} atuais (média)</strong>.<br />
-
-Nesse preço ainda podemos ter promoções, depende um pouco da ocasião.<br />
-          <br />
-E terá uma curva de produção como abaixo:<br />
-
-          <br />
-
-          <div id="divToPrint" >
-            <SolarBarChart 
-              id="currentChart" 
-              ref={(chart) => this.currentChart = chart} 
-              data={this.props.dataGraph}
-            />
-          </div>
-          
-Por favor visite nosso website para ter mais informações sobre energia solar:<br />
-
-http://integrahausenergiasolar.com.br/energia-solar-para-residencias/ <br />
-
-http://integrahausenergiasolar.com.br/vantagens-e-desvantagens-da-energia-solar/ <br />
-
-Obrigado pelo contato.
-Estamos a disposição para fazer uma visita e lhe explicar o funcionamento da energia solar pessoalmente.<br />
-
-Atenciosamente,<br />
-
+        <div id="divToPrint" >
+          <SolarBarChart 
+            id="currentChart" 
+            ref={(chart) => this.currentChart = chart} 
+            data={this.props.dataGraph}
+          />
         </div>
 
+        {this.costumerTextSecondPart()}
+
         <p>
-          <Button onClick={(e) => this.proposalButton()}> Envia Proposta »</Button>
+          <Button onClick={(e) => this.proposalButton()}>Salva Proposta »</Button>
+          <Button onClick={(e) => this.sendEmail()}>Enviar por email</Button>
           <Button onClick={(e) => this.printDocument()}>Salvar PDF</Button>
         </p>
 
--- js/source/config.js
+++ js/source/config.js
@@ -3,6 +3,7 @@ export let COSTUMERS_URL;
 export let LOGIN_URL;
 export let LOGOUT_URL;
 export let PROPOSAL_URL;
+export let PROPOSAL_EMAIL_URL;
 
 
 if (process.env.NODE_ENV === 'production') { 
@@ -10,9 +11,11 @@ if (process.env.NODE_ENV === 'production') {
   LOGIN_URL = '/users/loginclient';
   LOGOUT_URL = '/users/logoutclient';
   PROPOSAL_URL = '/proposals';
+  PROPOSAL_EMAIL_URL = '/proposals/email';
 } else {
   COSTUMERS_URL = 'http://127.0.0.1:3000/costumers/';
   LOGIN_URL = 'http://127.0.0.1:3000/users/loginclient/';
   LOGOUT_URL = 'http://127.0.0.1:3000/users/logoutclient';
   PROPOSAL_URL = 'http://127.0.0.1:3000/proposals';
+  PROPOSAL_EMAIL_URL = 'http://127.0.0.1:3000/proposals/email';
 }
